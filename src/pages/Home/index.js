04f20e102f82007baf38dd2b386f99ce1b3225aa
Hugo Vinícius de Lima Silva <hugovinicius145@gmail.com>
2020-03-27 16:02:17 -0300

    Iniciando com Redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
-import { MdAddShoppingCart } from 'react-icons/md'
+import { connect } from 'react-redux';
+import { MdAddShoppingCart } from 'react-icons/md';
 import api from '../../services/api';
 import { formatPrice } from '../../util/format';
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   }
 
   async componentDidMount() {
     const response = await api.get('products');
-    
+
     const data = response.data.map(product => ({
       ...product,
       priceFormatted: formatPrice(product.price),
     }))
     this.setState({ products: data });
+  }
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
   }
 
   render() {
@@ -28,17 +37,19 @@ export default class Home extends Component {
             <img src={product.image} alt={product.title}/>
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
-    
-            <button type="button">
+
+            <button type="button" onClick={() => this.handleAddProduct(product)}>
               <div>
                 <MdAddShoppingCart size={16} color="#FFF"/> 3
               </div>
-    
+
               <span>ADCIONAR AO CARRINHO</span>
             </button>
           </li>
-        ))}        
+        ))}
       </ProductList>
     );
   }
 }
+
+export default connect()(Home);
